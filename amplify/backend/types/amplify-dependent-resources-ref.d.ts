4965fbe236107675e27b6e124736cb22d7598992
h2m_kinoko <happy2mylife@gmail.com>
2024-04-08 13:51:01 +0900

    amplify push --yでAPIをデプロイ後

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "amplambdatest": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "amplambdatestbba3b2d0": {
       "AppClientID": "string",
